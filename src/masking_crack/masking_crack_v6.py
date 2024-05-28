330782da113628d26ce84cd6940ad10471af7502
Thanaphong007 <108317032+Thanaphong007@users.noreply.github.com>
2024-05-28 11:18:56 +0700

    3 line for 100 picture

--- src/masking_crack/masking_crack_v6.py
+++ src/masking_crack/masking_crack_v6.py
@@ -6,9 +6,9 @@ import os
 import numpy as np
 import sys
 
-images_path = "../../data/Palm/PalmAll"
+images_path = "data/Palm/PalmAll"
 
-mask_output_path = "../../output/dataset/"
+mask_output_path = "output/dataset/"
 
 # Initialize the main window
 win = Tk()
